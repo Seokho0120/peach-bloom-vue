0b7d27d4ee269ecc5d5c985e35ec892f41f833a8
seokho0120 <seokho0120@gmail.com>
2024-07-23 17:58:02 +0900

    feat: ItemLIstCard 추가

---
+++ src/components/items/ItemsListCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  imageUrl: string[];
+  brandName: string;
+  productName: string;
+  saleRate: number;
+  salePrice: number;
+}>();
+</script>
+
+<template>
+  <div>
+    <img :src="imageUrl[0]" alt="image" />
+
+    <div class="pt-3.5">
+      <div class="text-xs pb-1">{{ brandName }}</div>
+      <div class="text-xs pb-2">{{ productName }}</div>
+
+      <div class="flex gap-2">
+        <div class="text-[#ff4800]">{{ saleRate }}%</div>
+        <div class="font-bold">{{ salePrice }}</div>
+      </div>
+    </div>
+  </div>
+</template>
