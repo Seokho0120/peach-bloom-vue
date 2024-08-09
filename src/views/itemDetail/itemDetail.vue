53873703062b04a3e0fd261d890abe7c4ba2f9f3
seokho0120 <seokho0120@gmail.com>
2024-08-09 15:44:50 +0900

    feat: 아이템 디테일 작업중

--- src/views/itemDetail/itemDetail.vue
+++ src/views/itemDetail/itemDetail.vue
@@ -43,6 +43,20 @@ const ratingValue = computed(() => {
 
   return itemDetail.value?.reviewCount ? (itemDetail.value.reviewCount / max) * 5 : 0;
 });
+
+const number = ref(0);
+
+const quantity = ref(1); // 초기 수량
+
+const increaseQuantity = () => {
+  quantity.value++;
+};
+
+const decreaseQuantity = () => {
+  if (quantity.value > 1) {
+    quantity.value--;
+  }
+};
 </script>
 
 <template>
@@ -50,6 +64,7 @@ const ratingValue = computed(() => {
     <div class="flex-shrink-0">
       <Image :src="itemDetail?.imageUrl[0]" alt="Detail Image" width="564" class="w-full h-auto object-cover" />
     </div>
+
     <div class="w-full border-t-4 border-black">
       <div class="flex items-center justify-between py-6 text-2xl font-bold">
         <div>{{ itemDetail?.productName }}</div>
@@ -73,8 +88,99 @@ const ratingValue = computed(() => {
         <Rating v-model="ratingValue" readonly :cancel="false" />
         <p class="underline cursor-pointer">{{ itemDetail?.reviewCount }}</p>
       </div>
+
+      <div class="flex items-center font-bold gap-1">
+        <p class="text-2xl text-[#ff4801]">{{ itemDetail?.saleRate }}%</p>
+        <p class="text-2xl">{{ itemDetail?.consumerPrice }}</p>
+        <p class="text-lg">원</p>
+      </div>
+
+      <!-- TODO: 세일 아닐떄 보여주기 -->
+      <!-- <div class="flex items-center font-bold">
+        <p class="text-2xl">{{ itemDetail?.consumerPrice }}</p>
+        <p class="text-lg">원</p>
+      </div> -->
+
+      <div class="flex items-center gap-2">
+        <p>구매 적립금</p>
+        <!-- TODO: 구매급액의 3% ~ 5%로 측정하기 -->
+        <p class="text-red-400">최대 380 마일리지 적립 예정</p>
+      </div>
+
+      <div class="flex items-center gap-2">
+        <p>무이자 할부</p>
+        <p class="text-red-400">카드사별 할부 혜택 안내</p>
+      </div>
+
+      <div class="flex items-center gap-2">
+        <p>배송정보</p>
+        <p class="text-red-400">1일 이내 출고</p>
+      </div>
+
+      <div class="flex items-center gap-2">
+        <p>배송비</p>
+        <p>50,000원 이상 구매시 무료배송 제주/도서산간 1,000원 추가</p>
+      </div>
+
+      <div className="w-36 font-bold flex items-center justify-between border border-gray-200 gap-3 lg:gap-4">
+        <!-- <button
+          class="border-r flex items-center justify-center px-4 py-4"
+          @click="decreaseQuantity"
+          :disabled="quantity <= 1"
+        >
+          -
+        </button> -->
+        <button
+          class="border-r flex items-center justify-center px-4 py-4"
+          @click="decreaseQuantity"
+          :disabled="quantity <= 1"
+        >
+          <i class="pi pi-minus"></i>
+        </button>
+        <div>{{ quantity }}</div>
+        <!-- <button class="border-l flex items-center justify-center px-4 py-4" @click="increaseQuantity">+</button> -->
+
+        <button class="border-l flex items-center justify-center px-4 py-4" @click="increaseQuantity">
+          <i class="pi pi-plus"></i>
+        </button>
+      </div>
+
+      <div class="flex items-center gap-2">
+        <Button
+          label="장바구니 담기"
+          text
+          :pt="{
+            root: {
+              class: 'border-[1px] border-black text-black rounded-none text-sm w-full py-3 text-sm',
+            },
+          }"
+        />
+        <Button
+          label="바로 구매하기"
+          severity="contrast"
+          :pt="{
+            root: {
+              class: 'border-[1px] border-black rounded-none w-full py-3 text-sm',
+            },
+          }"
+        />
+      </div>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.quantity-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.quantity-display {
+  margin: 0 15px;
+  font-size: 1.5em;
+  font-weight: bold;
+}
+</style>
