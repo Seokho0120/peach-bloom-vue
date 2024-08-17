5da155e5c483e7ab8fbb54b977efeb3efef203e2
Seokho <seokho0120@gmail.com>
2024-08-18 01:42:28 +0900

    feat: cart 수량 적용, 총 결제금액 개선

--- src/views/cart/cart.vue
+++ src/views/cart/cart.vue
@@ -2,10 +2,23 @@
 import { ref, watch } from 'vue';
 import { useGetCartItemsList } from '@/composables/useCartItems';
 import type { CartItemListType } from '@/types/items.types';
+import type { DataTableRowSelectEvent } from 'primevue/datatable';
+
+interface ResultInfoType {
+  totalOrder: number;
+  totalShipping: number;
+  totalPayment: number;
+}
 
 const { data: cartItemList, isLoading, isError } = useGetCartItemsList();
-const selectedProduct = ref();
+
+const selectedProduct = ref<CartItemListType[]>([]);
 const quantityValues = ref<Record<string, number>>({}); // Record<Key, Value>
+const selectAll = ref(true);
+
+function buyItem(index: number) {
+  console.log('buyItem', index);
+}
 
 watch(
   cartItemList,
@@ -14,42 +27,93 @@ watch(
       newData.forEach((item) => {
         quantityValues.value[item.productId] = item.quantity;
       });
-      console.log('quantityValues.value', quantityValues.value);
 
       selectedProduct.value = [...newData];
-      console.log('selectedProduct.value', selectedProduct.value);
     }
   },
   { immediate: true },
 );
 
-function buyItem(index: any) {
-  console.log('buyItem', index);
+function onSelectAll() {
+  selectAll.value = !selectAll.value;
+
+  if (selectAll.value && cartItemList.value) {
+    selectedProduct.value = [...cartItemList.value];
+  } else {
+    selectedProduct.value = [];
+  }
 }
 
-watch(selectedProduct, () => {
-  console.log('selectedProduct.value', selectedProduct.value);
+const resultInfo = ref<ResultInfoType>({
+  totalOrder: 0,
+  totalShipping: 0,
+  totalPayment: 0,
 });
 
-const test1 = ref(true);
-function test() {
-  console.log('전체');
-  test1.value = !test1.value;
+const totalOrder = ref(0);
+const totalShipping = ref(0);
+const totalPayment = ref(0);
+
+function updateResultInfo() {
+  resultInfo.value.totalOrder = totalOrder.value;
+  resultInfo.value.totalShipping = totalShipping.value;
+  resultInfo.value.totalPayment = totalPayment.value;
 }
+
+watch(selectedProduct, (newVal) => {
+  if (newVal.length !== cartItemList.value?.length) {
+    selectAll.value = false;
+  } else {
+    selectAll.value = true;
+  }
+
+  const hasLowPriceItem = newVal.some((item) => {
+    const price = item.isSale ? item.salePrice : item.consumerPrice;
+    return price <= 30000;
+  });
+  totalShipping.value = hasLowPriceItem ? 3000 : 0;
+  totalOrder.value = 0;
+
+  newVal.forEach((item) => {
+    const quantity = quantityValues.value[item.productId] || 0;
+    const price = item.isSale ? item.salePrice : item.consumerPrice;
+
+    totalOrder.value += price * quantity;
+  });
+  totalPayment.value = totalOrder.value + totalShipping.value;
+
+  updateResultInfo();
+});
+
+// 수량 감지
+watch(
+  quantityValues,
+  (newQuantities) => {
+    totalOrder.value = 0;
+    selectedProduct.value.forEach((item) => {
+      const quantity = newQuantities[item.productId] || 0;
+      const price = item.isSale ? item.salePrice : item.consumerPrice;
+
+      totalOrder.value += price * quantity;
+    });
+    totalPayment.value = totalOrder.value + totalShipping.value;
+
+    updateResultInfo();
+  },
+  { deep: true },
+);
 </script>
 
 <template>
-  <div class="flex flex-col w-full max-w-[100rem] min-w-[50rem] mx-auto pb-8 px-12">
-    <!-- :select-all="test1"
-    @select-all-change="test()" -->
+  <div class="flex flex-col w-full max-w-[100rem] min-w-[50rem] mx-auto pb-40 px-12">
     <DataTable
       class="border-t-4 border-black"
       v-model:selection="selectedProduct"
       :value="cartItemList"
       data-key="productId"
-      paginator
-      :rows="5"
       :loading="isLoading"
+      @select-all-change="() => onSelectAll()"
+      :select-all="selectAll"
     >
       <template #empty>
         <p class="text-2xl font-bold flex justify-center items-center p-12">장바구니에 담은 상품이 없습니다.</p>
@@ -174,6 +238,77 @@ function test() {
               <span class="text-xs">원</span>
             </div>
             <div v-else>무료배송</div>
+          </div>
+        </template>
+      </Column>
+    </DataTable>
+
+    <DataTable
+      class="border-t-4 border-black"
+      :loading="isLoading"
+      :value="[resultInfo]"
+      :pt="{
+        root: {
+          class: 'mt-24',
+        },
+        header: {
+          class: 'bg-red-400',
+        },
+        thead: {
+          class: 'bg-red-400',
+        },
+      }"
+    >
+      <Column
+        header="총 주문금액"
+        :pt="{
+          headerCell: {
+            class: 'py-4 px-6 text-lg font-bold',
+            // class: 'flex flex-col items-center py-4 px-6 text-lg font-bold',
+          },
+          bodyCell: {
+            class: 'py-8',
+          },
+        }"
+      >
+        <template #body="{ data }">
+          <div class="flex flex-col">
+            <div class="flex items-baseline">
+              <p class="text-3xl font-bold">{{ data.totalOrder }}</p>
+              <span class="font-semibold">원</span>
+            </div>
+          </div>
+        </template>
+      </Column>
+
+      <Column
+        header="총 배송비"
+        :pt="{
+          headerCell: {
+            class: 'py-4 px-6 text-lg font-bold',
+          },
+        }"
+      >
+        <template #body="{ data }">
+          <div class="flex items-baseline">
+            <p class="text-3xl font-bold">{{ data.totalShipping }}</p>
+            <span class="font-semibold">원</span>
+          </div>
+        </template>
+      </Column>
+
+      <Column
+        header="총 결제금액"
+        :pt="{
+          headerCell: {
+            class: 'py-4 px-6 text-lg font-bold',
+          },
+        }"
+      >
+        <template #body="{ data }">
+          <div class="flex items-baseline">
+            <p class="text-3xl font-bold">{{ data.totalPayment }}</p>
+            <span class="font-semibold">원</span>
           </div>
         </template>
       </Column>
